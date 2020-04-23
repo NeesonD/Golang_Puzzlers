4a72ebf460270bcbd06300de955c65314c507404
neeson <d1043358774@gmail.com>
2020-04-23 23:42:32 +0800

    笔记

--- src/puzzlers/article2/q1/demo2.go
+++ src/puzzlers/article2/q1/demo2.go
@@ -8,10 +8,12 @@ import (
 var name string
 
 func init() {
+	// main 如何接受参数
 	flag.StringVar(&name, "name", "everyone", "The greeting object.")
 }
 
 func main() {
+	// 解析命令参数
 	flag.Parse()
 	fmt.Printf("Hello, %s!\n", name)
 }
